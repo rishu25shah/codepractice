24c54760171f781a6a843fc6b641036bb87aaa18
Rishu <rishu25shah@gmail.com>
2026-09-26 21:18:43 +0530

    Add day4/pattern1.py

---
+++ day4/pattern1.py
@@ -0,0 +1,12 @@
+def square(n):
+    for i in range(n):
+        for j  in range(n):
+            print("*",end="")
+        print()
+
+def main():
+    n=int(input())
+    square(n)
+
+if __name__=="__main__":
+    main()
