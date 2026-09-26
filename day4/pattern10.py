d93c17d9dca70419d38baa9b921ad13fa9912be7
Rishu <rishu25shah@gmail.com>
2026-09-26 21:18:53 +0530

    Add day4/pattern10.py

---
+++ day4/pattern10.py
@@ -0,0 +1,19 @@
+def hollowrectangle(r,c):
+    for i in range(r):
+        for j in range(c):
+            if i==0 or i==r-1 or j==0 or j==c-1:
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print()
+            
+
+        
+
+def main():
+    r=int(input())
+    c=int(input())
+    hollowrectangle(r,c)
+
+if __name__=="__main__":
+    main()
