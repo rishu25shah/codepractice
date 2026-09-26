53b5be6727f0e943f88b605348a5e2f6a9ba72dd
Rishu <rishu25shah@gmail.com>
2026-09-26 21:18:53 +0530

    Add day4/pattern4.py

---
+++ day4/pattern4.py
@@ -0,0 +1,12 @@
+def numbertriangle(n):
+    for i in range(n):
+        for j  in range(i+1):
+            print(j+1,end="")
+        print()
+
+def main():
+    n=int(input())
+    numbertriangle(n)
+
+if __name__=="__main__":
+    main()
